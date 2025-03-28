23bac67aa513a3715f6442a6f68d075147633f63
Harsh-2909 <harsh29092000@gmail.com>
2025-03-28 12:47:51 +0530

    refactor: Updates main.go file

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Harsh-2909/hermes-go/agent"
 	"github.com/Harsh-2909/hermes-go/models"
 	openai "github.com/Harsh-2909/hermes-go/models/openai"
+	"github.com/Harsh-2909/hermes-go/tools"
 
 	"github.com/joho/godotenv"
 )
@@ -25,43 +26,67 @@ func main() {
 		ApiKey: os.Getenv("OPENAI_API_KEY"),
 		Id:     "gpt-4o-mini",
 	}
-	model.Init()
 
+	// Building tool manually
+	// params := map[string]interface{}{
+	// 	"type": "object",
+	// 	"properties": map[string]interface{}{
+	// 		"a": map[string]interface{}{"type": "number"},
+	// 		"b": map[string]interface{}{"type": "number"},
+	// 	},
+	// 	"required": []string{"a", "b"},
+	// }
+	// tool := tools.NewTool("Calculate", "Calculate the sum of two numbers", params, func(ctx context.Context, args string) (string, error) {
+	// 	var input struct{ A, B int }
+	// 	if err := json.Unmarshal([]byte(args), &input); err != nil {
+	// 		return "", err
+	// 	}
+	// 	return fmt.Sprintf("%d", input.A+input.B), nil
+	// })
+	// tool.Tools()[0].Execute(context.Background(), `{"a": 2, "b": 3}`)
+
+	calcTools := &tools.CalculatorTools{
+		EnableAll: true,
+	}
 	agent := &agent.Agent{
 		Model:       model,
 		Description: "You are a helpful assistant.",
 		Markdown:    true,
+		DebugMode:   false,
+		// Tools:       []tools.ToolKit{tool},
+		Tools: []tools.ToolKit{calcTools},
 	}
-	agent.Init()
 
 	// Non Streaming Example
 	ctx := context.Background()
-	response, err := agent.Run(ctx, "Hello, how are you?")
+	response, err := agent.Run(ctx, "Can you say hello and add 267383 and 123456?")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	fmt.Println("Assistant:", response.Data)
+	fmt.Printf("\nMessages:\n\n%+v\n", agent.Messages)
 
 	// Streaming Example
-	// ctx1 := context.Background()
-	// stream, err := agent.RunStream(ctx1, "What is the meaning of life?")
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// fmt.Println("Assistant is thinking...")
-	// for resp := range stream {
-	// 	if resp.Event == "chunk" {
-	// 		fmt.Print(resp.Data)
-	// 	} else if resp.Event == "complete" {
-	// 		fmt.Print("\nAssistant is Done.")
-	// 		break
-	// 	} else if resp.Event == "error" {
-	// 		fmt.Println("\nError:", resp.Data)
-	// 		break
-	// 	}
-	// }
+	ctx1 := context.Background()
+	stream, err := agent.RunStream(ctx1, "Can you say hello and add 267383 and 123456?")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Assistant is thinking...")
+	for resp := range stream {
+		if resp.Event == "chunk" {
+			fmt.Print(resp.Data)
+		} else if resp.Event == "end" {
+			fmt.Print("\nAssistant is Done.")
+			break
+		} else if resp.Event == "error" {
+			fmt.Println("\nError:", resp.Data)
+			break
+		}
+	}
+	fmt.Printf("\nMessages:\n\n%+v\n", agent.Messages)
 
 	// Multimodal Example
 	// Image content
@@ -69,7 +94,11 @@ func main() {
 	image := &models.Image{
 		URL: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-10th-gen-finish-select-202212-pink-wifi_FMT_WHH?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1670856074755",
 	}
-	response, err = agent.Run(ctx2, "What is in this image?", image)
+	audio := &models.Audio{
+		URL: "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3",
+	}
+	response, err = agent.Run(ctx2, "What is in this image?", image, audio)
+	// response, err = agent.Run(ctx2, "What is in this image?", image)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
