5d56d62339d0cad54dd8bdace463e590635652e1
Harsh-2909 <harsh29092000@gmail.com>
2025-04-13 17:46:23 +0530

    feat: Adds pterm slog handler instead of default slog handler for better log readability

--- utils/log.go
+++ utils/log.go
@@ -2,7 +2,8 @@ package utils
 
 import (
 	"log/slog"
-	"os"
+
+	"github.com/pterm/pterm"
 )
 
 // Logger is the global logger instance used throughout the application.
@@ -10,20 +11,18 @@ var Logger *slog.Logger
 
 func init() {
 	// Initialize with default settings (Info level)
-	Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	}))
+	handler := pterm.NewSlogHandler(&pterm.DefaultLogger)
+	Logger = slog.New(handler)
 }
 
 // InitLogger can be called later to change the logger configuration
 func InitLogger(debug bool) {
-	var level slog.Level
+	// var level slog.Level
 	if debug {
-		level = slog.LevelDebug
-	} else {
-		level = slog.LevelInfo
+		pterm.DefaultLogger.Level = pterm.LogLevelDebug
+		// level = slog.LevelDebug
 	}
-	Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	}))
+	// Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	// 	Level: level,
+	// }))
 }
